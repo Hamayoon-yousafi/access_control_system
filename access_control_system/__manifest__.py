027ddb25ec993e53965662a5e123c60ca30b6141
Hamayoon-yousafi <hamayoonyousafi555@gmail.com>
2024-02-27 11:08:37 +0430

    project scaffold and setup

---
+++ access_control_system/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': "Access Control System",
+
+    'summary': """
+        Access Control System for gate pass.    
+    """,
+
+    'description': """
+        Access Control System for gate pass to manage users and allow them based on given barcode.
+    """,
+
+    'author': "HAmayoon Yousafi",
+    'website': "",
+
+    'category': 'Uncategorized',
+    'version': '0.1',
+
+    'depends': ['base'],
+
+    'data': [
+        'security/ir.model.access.csv',
+        'views/views.xml',
+        'views/templates.xml',
+    ]
+}
